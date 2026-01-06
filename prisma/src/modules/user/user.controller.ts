35d7363f07cc0ff4ddb688db6a0e1ee8f85b5a54
Md.Arifur Rahman sajid <159644399+arifurrahmansajid@users.noreply.github.com>
2026-01-06 14:16:11 +0600

    Add files via upload

---
+++ prisma/src/modules/user/user.controller.ts
@@ -0,0 +1,26 @@
+import { RequestHandler } from "express";
+import { prisma } from "../../lib/prisma";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { userService } from "./user.service";
+
+const register: RequestHandler = async (req, res) => {
+  const payload = req.body;
+
+  const user = await userService.register(payload);
+
+  res.send({ message: "Registered Successfully", data: user });
+};
+
+const login: RequestHandler = async (req, res) => {
+  const { email, password } = req.body;
+
+  const token = await userService.login(email, password);
+
+  res.send({ message: "Logged in successfully", token });
+};
+
+export const userController = {
+  register,
+  login,
+};
