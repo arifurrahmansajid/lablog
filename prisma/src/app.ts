47905ba6e39acba44a448a8f8ce0721cb1ab5fa0
Md.Arifur Rahman sajid <159644399+arifurrahmansajid@users.noreply.github.com>
2026-01-06 14:16:11 +0600

    Add files via upload

---
+++ prisma/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use("/api/v1", routes);
+
+export default app;
---
+++ prisma/src/middleware/auth.ts
@@ -0,0 +1,39 @@
+// (req, res, next)
+
+import { Request, Response, NextFunction } from "express";
+import { Role } from "../generated/prisma/enums";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import routes from "../routes";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: JwtPayload;
+    }
+  }
+}
+
+const auth = (roles?: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) res.send("Please provide token");
+
+    try {
+      const decoded = jwt.verify(token as string, "very secret");
+
+      if (!decoded) return res.send("Unauthorized");
+
+      req.user = decoded as JwtPayload;
+
+      if (roles && !roles.includes(req.user.role)) {
+        return res.send("Forbidden");
+      }
+
+      next();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export default auth;
